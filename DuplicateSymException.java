5bead898eaec7306bf5ba2da60263e41d961f41a
Nam <npham6@wisc.edu>
2018-09-12 14:59:18 -0500

    adding exception files

---
+++ DuplicateSymException.java
@@ -0,0 +1,2 @@
+ public class DuplicateSymException extends Exception {
+    }
---
+++ EmptySymTableException.java
@@ -0,0 +1,5 @@
+ public class EmptySymTableException extends Exception {
+	 	public EmptySymTableException(){
+	 		
+		}
+    }
